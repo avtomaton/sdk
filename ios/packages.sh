d297367b1bb8fa9c7cdb2895113f09f4e08b8941
Victor Pogrebnyak <avtomaton@gmail.com>
2016-02-29 10:38:12 -0500

    necessary packages for proper SDK build

---
+++ ios/packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install cmake
+brew install automake
+brew install autoconf
+brew install libtool
