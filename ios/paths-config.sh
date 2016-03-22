fd771700eabc63a471138f2fdb5b46fb76a09138
Viktor Pogrebniak <avtomaton@gmail.com>
2016-03-22 17:06:11 +0300

    COMMON_BUILD_PATH -> COMMON_BUILD_DIR

--- ios/paths-config.sh
+++ ios/paths-config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-COMMON_BUILD_PATH=`pwd`
-case $COMMON_BUILD_PATH in  
+COMMON_BUILD_DIR=`pwd`
+case $COMMON_BUILD_DIR in  
      *\ * )
            echo "Your path contains whitespaces, which is not supported by 'make install'."
            exit 1
           ;;
 esac
 
-TARBALL_DIR=$COMMON_BUILD_PATH/downloads
+TARBALL_DIR=$COMMON_BUILD_DIR/downloads
