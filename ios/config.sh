49f796274fa0c4c8526de172f1e73b623c718a2a
Viktor Pogrebniak <avtomaton@gmail.com>
2016-03-24 02:38:08 +0300

    ios: unified config

--- ios/config.sh
+++ ios/config.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-export XCODE_XCCONFIG_FILE=$SCRIPT_DIR/no-code-sign.xcconfig
+CPPSTD=c++11    #c++89, c++99, c++14
+STDLIB=libc++   # libstdc++
+COMPILER=clang++
+PARALLEL_MAKE=7   # how many threads to build
+
+#BITCODE="-fembed-bitcode"  # Uncomment this line for Bitcode generation
 
 ARM_DEV_CMD="xcrun --sdk iphoneos"
 SIM_DEV_CMD="xcrun --sdk iphonesimulator"
 OSX_DEV_CMD="xcrun --sdk macosx"
+IOS_MIN_VERSION=7.0
+
+COMMON_BUILD_DIR=`pwd`
+case $COMMON_BUILD_DIR in  
+     *\ * )
+           echo "Your path contains whitespaces, which is not supported by 'make install'."
+           exit 1
+          ;;
+esac
+
+TARBALL_DIR=$COMMON_BUILD_DIR/downloads
+
+export XCODE_XCCONFIG_FILE=$SCRIPT_DIR/no-code-sign.xcconfig
 
 #IPHONE_SDKVERSION=`xcodebuild -showsdks | grep iphoneos | egrep "[[:digit:]]+\.[[:digit:]]+" -o | tail -1`
 IPHONE_SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`
@@ -12,11 +30,11 @@ IPHONEOS_PLATFORM=`xcrun --sdk iphoneos --show-sdk-platform-path`
 IPHONEOS_SYSROOT=`xcrun --sdk iphoneos --show-sdk-path`
 IPHONESIMULATOR_PLATFORM=`xcrun --sdk iphonesimulator --show-sdk-platform-path`
 IPHONESIMULATOR_SYSROOT=`xcrun --sdk iphonesimulator --show-sdk-path`
-
-OS_RELEASE=`uname -r`
-
 OSX_SDKVERSION=`xcrun -sdk macosx --show-sdk-version`
 XCODE_ROOT=`xcode-select -print-path`
+OS_RELEASE=`uname -r`
+ARCH_POSTFIX=darwin$OS_RELEASE
+
 if [ ! -d "$XCODE_ROOT" ]; then
   echo "xcode path is not set correctly set: '$XCODE_ROOT' does not exist (most likely because of xcode > 4.3)"
   echo "run"
