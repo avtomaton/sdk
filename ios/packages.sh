1c3d94ad66c81b4eed5015aff5693e3879c9657d
Viktor Pogrebniak <avtomaton@gmail.com>
2016-03-24 19:13:05 +0300

    ios: += scripts exec mode, packages checker fix

--- ios/packages.sh
+++ ios/packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew -h || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install git
-brew install cmake
-brew install automake
-brew install autoconf
-brew install libtool
+git help || brew install git
+cmake -h || brew install cmake
+automake --help || brew install automake
+autoconf --help || brew install autoconf
+brew list | grep libtool || brew install libtool
